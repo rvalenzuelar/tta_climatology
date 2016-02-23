3e7779c6189c4887d74ff282fe516e6244d732ba
rvalenzuelar <raul.valenzuela@colorado.edu>
2016-02-22 18:54:36 -0700

    first commit

---
+++ parse_data.py
@@ -0,0 +1,143 @@
+
+
+import scipy.io as sio
+import numpy as np
+import pandas as pd
+import os
+
+
+base_dir=os.path.expanduser('~')
+
+
+def bby(year=None, freq=None):
+
+	y=str(year)[-2:]
+	fbby=base_dir+'/SURFACE/climatology/BBY'+y+'_Sfcmet'
+	matbby=sio.loadmat(fbby)
+	sfcbby=matbby['Sfcmet_bby']
+	# cols=sfc.dtype.names
+	date, tempc, rh, pmb, wspd,wdir,precip=[],[],[],[],[],[],[]
+	for n in range(sfcbby.size):
+		date.append(datenum_to_datetime(sfcbby[:,n][0][0][0][0]))
+		tempc.append(sfcbby[:,n][0][1][0][0])
+		rh.append(sfcbby[:,n][0][2][0][0])
+		pmb.append(sfcbby[:,n][0][3][0][0])
+		wspd.append(sfcbby[:,n][0][4][0][0])
+		wdir.append(sfcbby[:,n][0][5][0][0])
+		precip.append(sfcbby[:,n][0][6][0][0])
+	d={'tempc':tempc,'rh':rh,'pmb':pmb, 
+		'wspd':wspd,'wdir':wdir,'precip':precip}
+	BBY=pd.DataFrame(data=d, index=date)
+	if year==2001:
+		'2001 has weird values beginning the mat file'
+		BBY=BBY.ix[67:]
+	BBY=quality_control(BBY)
+
+	if freq:
+		BBY=get_statistical(BBY,minutes=freq)
+	else:
+		return BBY
+
+def get_statistical(df, minutes=None):
+	''' 
+	sum for precip
+	mean for wspd, wdir
+	'''
+	grp=pd.TimeGrouper(str(minutes)+'T')
+	precip=df.precip.groupby(grp).sum()
+	
+
+
+def czd(year=None):
+
+	y=str(year)[-2:]
+	fczd=base_dir+'/SURFACE/climatology/avg60_CZC'+y+'_nortype'
+	matczd=sio.loadmat(fczd)
+	rainczd=matczd['avg_czc_sprof_rtype_precip60'][0]
+	begd=matczd['avg_czc_sprof_begdayt60'][0]
+	# endd=matczd['avg_czc_sprof_enddayt60'][0]
+	date=[]
+	for n in range(begd.size):
+		date.append(datenum_to_datetime(begd[n]))
+	d={'precip':rainczd}
+	CZD=pd.DataFrame(data=d, index=date)
+	CZD=quality_control(CZD)
+
+	return CZD
+
+def quality_control(df):
+
+	if 'precip' in df:
+		cond=df.precip<0.
+		df.precip[cond]=np.nan
+
+	if 'wspd' in df:
+		std=df.wspd.std()
+		cond=(df.wspd<0.)
+		df.wspd[cond]=np.nan
+
+	if 'wdir' in df:
+		cond=(df.wdir<0.) | (df.wdir>360.)
+		df.wdir[cond]=np.nan
+
+	if 'rh' in df:
+		cond=(df.rh<0) |  (df.rh>100)
+		df.rh[cond]=np.nan
+
+	return df
+
+def datenum_to_datetime(datenum):
+	from datetime import datetime, timedelta
+	"""
+	Convert Matlab datenum into Python datetime.
+	:param datenum: Date in datenum format
+	:return:        Datetime object corresponding to datenum.
+
+	source: https://gist.github.com/vicow
+	"""
+	days = datenum % 1
+	hours = days % 1 * 24
+	minutes = hours % 1 * 60
+	seconds = minutes % 1 * 60
+	return datetime.fromordinal(int(datenum)) \
+	       + timedelta(days=int(days)) \
+	       + timedelta(hours=int(hours)) \
+	       + timedelta(minutes=int(minutes)) \
+	       + timedelta(seconds=round(seconds)) \
+	       - timedelta(days=366)
+
+
+def average_wind(wdir, wspd):
+	'''
+	source:
+	http://www.intellovations.com/2011/01/16/
+	wind-observation-calculations-in-fortran-and-python/
+
+	wdir  -- array of wind directions
+	wspd  -- array of wind speeds   	
+	'''
+
+	import cmath 
+	import math
+
+	wind_vector_sum = None
+	wdir+=180
+	wdir=np.radians(wdir)
+	n_wind=len(wspd)
+
+	if n_wind >=2:
+		for i in range(0, n_wind):
+			wind_polar = cmath.rect(wspd[i], wdir[i] - math.pi)
+			if wind_vector_sum is None:
+				wind_vector_sum = wind_polar
+			else:
+				wind_vector_sum += wind_polar
+
+		r, phi = cmath.polar(wind_vector_sum / n_wind)
+		av_wdir = np.round(math.degrees(phi),1)
+		if av_wdir<0:
+			av_wdir+=360
+		av_wspd = int(round(r*10))/10.0
+		return av_wdir,av_wspd
+	else:
+		return None, None
---
+++ tta_climatology.py
@@ -0,0 +1,18 @@
+
+import parse_data
+
+
+# for y in [1998]+range(2001,2013):
+# 	print y
+# 	bby=parse_data.bby(y)
+# 	czd=parse_data.czd(y)
+# 	print 'ok'
+
+bby=parse_data.bby(1998)
+czd=parse_data.czd(1998)
+
+
+
+
+
+
