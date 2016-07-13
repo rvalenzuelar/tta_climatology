4aef41a1666650aa73a3f42268cf77ddc97808b9
raul <raul.valenzuela@colorado.edu>
2016-07-13 13:23:30 -0600

    last update

--- print_beg_end_dates.py
+++ print_beg_end_dates.py
@@ -12,9 +12,9 @@ years=[1998]+range(2001,2013)
 for y in years:
 
 	tta=tta_analysis(y)
-	tta.start_df(wdir_surf=125,wdir_wprof=170,rain_bby=0.25,nhours=5)
-	beg=tta.include_dates[0].strftime('%H%M %d %b %Y')
-	end=tta.include_dates[-1].strftime('%H%M %d %b %Y')
+	tta.start_df(wdir_surf=125,wdir_wprof=170,rain_bby=0.25,nhours=2)
+	beg=tta.include_dates[0].strftime('%H%M UTC %d %b %Y')
+	end=tta.include_dates[-1].strftime('%H%M UTC %d %b %Y')
 	year = tta.include_dates[-1].year
 	txt='{} {} {}'
 	print(txt.format(year,beg,end))
