37d8f18b14961c0c38671bc93d72f994a61af0f4
Raul Valenzuela <raul.valenzuela@colorado.edu>
2016-09-09 15:49:12 -0600

    first commit

---
+++ windprof_tta_sensit.py
@@ -0,0 +1,121 @@
+"""
+
+    Raul Valenzuela
+    raul.valenzuela@colorado.edu
+
+"""
+
+import parse_data
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import collections
+from tta_analysis2 import tta_analysis
+from rv_utilities import pandas2stack
+from matplotlib import rcParams
+
+rcParams['xtick.labelsize'] = 15
+rcParams['ytick.labelsize'] = 15
+rcParams['axes.labelsize'] = 15
+rcParams['ytick.color'] = (0.8, 0.8, 0.8)
+rcParams['mathtext.default'] = 'sf'
+
+sns.set_style('whitegrid')
+
+
+def sin(arg):
+    return np.sin(np.radians(arg))
+
+
+def cos(arg):
+    return np.cos(np.radians(arg))
+
+
+" creates one param per nhour "
+thres = [112, 126, 140, 154, 168]
+params = [{'wdir_thres': tr,
+           'wdir_layer': [0, 500],
+           'rain_czd':   0.25,
+           'nhours':     1
+           } for tr in thres]
+
+
+# years = [1998]
+years = [1998]+range(2001, 2013)
+
+U = pd.Series(name='U-comp')
+V = pd.Series(name='V-comp')
+
+try:
+    results
+
+except NameError:
+
+    results = collections.OrderedDict()
+    for tr in thres:
+        results[tr] = {'U': U, 'V': V}
+
+    for year in years:
+        for p in params:
+            tta = tta_analysis(year=year)
+
+            tta.start_df_layer(**p)
+            tta_dates = tta.tta_dates
+
+            " parse surface and profile obs "
+            bby = parse_data.surface('bby', year=year)
+            wpr = parse_data.windprof(year=year)
+
+            wpr_tta = wpr.dframe.loc[tta_dates]
+            wdr_tta = wpr_tta['wdir']
+            wsp_tta = wpr_tta['wspd']
+
+            bby_tta = bby.dframe.loc[tta_dates]
+
+            " append surface values to windprof "
+            surf_wsp = iter(bby_tta.wspd.values.tolist())
+            surf_wdr = iter(bby_tta.wdir.values.tolist())
+
+            wsp_tta = wsp_tta.map(lambda x: [surf_wsp.next()] + x)
+            wdr_tta = wdr_tta.map(lambda x: [surf_wdr.next()] + x)
+
+            " compute components "
+            wd_sin = wdr_tta.map(lambda x: sin(x))
+            wd_cos = wdr_tta.map(lambda x: cos(x))
+            u = -1 * wsp_tta.multiply(wd_sin)
+            v = -1 * wsp_tta.multiply(wd_cos)
+
+            " append to big dataframe "
+            thres = p['wdir_thres']
+            results[thres]['U'] = results[thres]['U'].append(u)
+            results[thres]['V'] = results[thres]['V'].append(v)
+
+    print('Done')
+
+fig, ax = plt.subplots(1, 2, sharey=True)
+y = [0]
+y = np.append(y, wpr.hgt)
+
+for thres in results.keys():
+
+    U_stack = np.squeeze(pandas2stack(results[thres]['U'])).T
+    V_stack = np.squeeze(pandas2stack(results[thres]['V'])).T
+
+    U_mean = np.nanmean(U_stack, axis=0)
+    V_mean = np.nanmean(V_stack, axis=0)
+
+    ax[0].plot(U_mean, y, label=str(thres)+'$^{\circ}$')
+    ax[1].plot(V_mean, y)
+
+ax[0].set_ylabel('Altitude [m] MSL')
+ax[0].legend(loc=0, fontsize=15)
+ax[0].text(0,4100,'U-comp', ha='center', fontsize=15)
+ax[0].set_xlabel('$[m\,s^{-1}]$')
+
+ax[1].text(6,4100,'V-comp', ha='center', fontsize=15)
+ax[1].set_xlabel('$[m\,s^{-1}]$')
+
+plt.subplots_adjust(bottom=0.15)
+
+plt.show()
