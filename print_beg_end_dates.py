54f1f16c533307dceef9262ee7344b55b3cf89d3
Raul Valenzuela <raul.valenzuela@colorado.edu>
2016-09-11 10:10:30 -0600

    using param dictionary

--- print_beg_end_dates.py
+++ print_beg_end_dates.py
@@ -4,7 +4,20 @@ Created on Thu Jun 16 10:09:25 2016
 
 @author: raul
 """
-from tta_analysis import tta_analysis
+from tta_analysis2 import tta_analysis
+
+# params=dict(wdir_surf  = 130,
+#             wdir_wprof = 170,
+#             rain_bby   = None,
+#             rain_czd   = 0.25,
+#             nhours     = 1)
+
+params=dict(wdir_surf  = 360,
+            wdir_wprof = 360,
+            rain_bby   = None,
+            rain_czd   = None,
+            nhours     = 1)
+
 
 #years=[1998]
 years = [1998]+range(2001,2013)
@@ -13,11 +26,7 @@ print(template.format('Season','Beg','End','Hours'))
 thours = 0
 for y in years:
     tta = tta_analysis(y)
-    tta.start_df(wdir_surf  = 130,
-                 wdir_wprof = 170,
-                 rain_bby   = None,
-                 rain_czd   = 0.25,
-                 nhours     = 1)
+    tta.start_df(**params)
     year = tta.include_dates[-1].year
     beg = tta.include_dates[0].strftime('%H%M UTC %d %b %Y')
     end = tta.include_dates[-1].strftime('%H%M UTC %d %b %Y')
